470f4e62f35490bb0a4cfbbc1cc0f2c9520a6ea9
Ranga Munasinghe <rangamunasinghe94@gmail.com>
2022-04-08 17:46:06 -0400

    pushing database design

--- java/database/database.sql
+++ java/database/database.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cards, decks, users;
+
 
 DROP SEQUENCE IF EXISTS seq_user_id;
 
@@ -11,12 +12,33 @@ CREATE SEQUENCE seq_user_id
   CACHE 1;
 
 
+
 CREATE TABLE users (
 	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
 	username varchar(50) UNIQUE NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE decks (
+	deck_id serial NOT NULL,
+	user_id int NOT NULL,
+	deck_name varchar(50) NOT NULL,
+	CONSTRAINT PK_deck PRIMARY KEY (deck_id),
+	CONSTRAINT FK_deck_user FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE cards (
+	card_id serial NOT NULL,
+	deck_id int NOT NULL,
+	front_text varchar(500) NOT NULL,
+	back_text varchar(500) NOT NULL,
+	keywords varchar(50) NOT NULL,
+	user_id int NOT NULL,
+	CONSTRAINT PK_card PRIMARY KEY (card_id),
+	CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users (user_id),
+	CONSTRAINT FK_deck FOREIGN KEY(deck_id) REFERENCES decks(deck_id)
 );
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
